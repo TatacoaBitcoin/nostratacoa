184a5d9d4c87a251c68e9a22eded70e9d205b293
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-06-19 19:18:09 -0500

    added material icons

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {Home, Market, Social, Settings} from './src/screens';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="wallet" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Market"
+          component={Market}
+          options={{
+            tabBarLabel: 'Market',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="shopping" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Social"
+          component={Social}
+          options={{
+            tabBarLabel: 'Social',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="comment-text-multiple" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({color, size}) => (
+              <Icon name="cog" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
