5ca7a960440c0ebdb08f2d336a88a08a82c78854
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-06-22 11:43:58 -0500

    added custom hook to generate random words

--- src/screens/CreateWallet.js
+++ src/screens/CreateWallet.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {Text} from 'react-native';
 
 import {ScreenTemplate} from '../atoms';
+import {useRecoveryWords} from '../hooks/useRecoveryWords';
 
 const CreateWallet = () => {
+  const {randomWords, generateWords, generateSeed} = useRecoveryWords();
+
+  console.log(randomWords);
+
   return (
     <ScreenTemplate>
       <Text>CreateWallet</Text>
