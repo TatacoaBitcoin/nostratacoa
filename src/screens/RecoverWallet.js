d4ce4c72d568bddc0670178b58a71b100b7deb1f
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-06-29 11:20:39 -0500

    validates word input

--- src/screens/RecoverWallet.js
+++ src/screens/RecoverWallet.js
@@ -9,21 +9,51 @@ import {
 } from 'react-native';
 
 import {ScreenTemplate} from '../atoms';
+import {isWordValid} from '../utils';
 
 const RecoverWallet = ({navigation}) => {
-  const [word, setWord] = useState();
+  const [words, setWords] = useState(Array(12).fill(''));
+  const [wordStatus, setWordStatus] = useState(Array(12).fill(''));
+
+  const handleInputChange = (value, index) => {
+    const isValid = isWordValid(value);
+    const updateValidWords = [...wordStatus];
+    if (value.length >= 3 && isValid) {
+      updateValidWords[index] = 'success';
+    } else if (value.length >= 3 && !isValid) {
+      updateValidWords[index] = 'danger';
+    } else if (value.length < 3) {
+      updateValidWords[index] = '';
+    }
+    setWordStatus(updateValidWords);
+    const updatedWords = [...words];
+    updatedWords[index] = value;
+    setWords(updatedWords);
+  };
 
   return (
     <ScreenTemplate>
       <ScrollView>
         <Text>Enter your 12-word phrase</Text>
-        {[...Array(12)].map((item, idx) => (
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputLabel}>{idx + 1}.</Text>
+        {words.map((word, idx) => (
+          <View style={styles.inputContainer} key={idx}>
+            <Text
+              style={[
+                styles.inputLabel,
+                wordStatus[idx] === 'success' && styles.success,
+                wordStatus[idx] === 'danger' && styles.danger,
+              ]}>
+              {idx + 1}.
+            </Text>
             <TextInput
-              style={styles.input}
-              onChangeText={setWord}
+              style={[
+                styles.input,
+                wordStatus[idx] === 'success' && styles.success,
+                wordStatus[idx] === 'danger' && styles.danger,
+              ]}
+              onChangeText={value => handleInputChange(value, idx)}
               value={word}
+              autoCapitalize={'none'}
             />
           </View>
         ))}
@@ -40,13 +70,20 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 20,
+    marginBottom: 20,
+  },
+  inputLabel: {
+    width: 20,
   },
   input: {
     borderBottomWidth: 1,
     width: '50%',
+    paddingBottom: 5,
   },
-  inputLabel: {
-    width: 20,
+  success: {
+    color: 'green',
+  },
+  danger: {
+    color: 'red',
   },
 });
